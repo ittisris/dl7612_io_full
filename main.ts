1250ed3d8fffe563c99b493c20c0334b4bb6162f
Itti Srisumalai <i2ktel@hotmail.com>
2020-05-18 07:54:30 +0700

    update

--- main.ts
+++ main.ts
@@ -1,51 +1,60 @@
+function extractPayload (response: string) {
+    rxPayload = cayenneLPP.extractPayloadStr(response, "", ":")
+    if (rxPayload.length > 0) {
+        if ("+NSMI".compare(rxPayload[0]) == 0) {
+            if (parseInt(rxPayload[1]) == 1) {
+                basic.showIcon(IconNames.Happy)
+                connected = true
+                lastupdated = input.runningTime()
+                serial.writeLine("AT+SLEEP")
+            } else {
+                basic.showIcon(IconNames.Sad)
+            }
+        } else {
+            if ("+NNMI".compare(rxPayload[0]) == 0) {
+                rxPayload = cayenneLPP.extractPayloadStr(response, "", ",")
+                if (rxPayload.length > 1) {
+                    basic.showArrow(ArrowNames.South)
+                    cayenneLPP.lpp_update(rxPayload[1])
+                }
+            } else {
+                if ("+CGATT".compare(rxPayload[0]) == 0) {
+                    connected = true
+                    lastupdated = input.runningTime()
+                    basic.showIcon(IconNames.Heart)
+                }
+            }
+        }
+        basic.pause(200)
+        basic.clearScreen()
+    }
+}
 function resetModule () {
-    if (input.runningTime() > lastupdated + 60000) {
+    if (input.runningTime() > lastupdated + maxDelay) {
+        basic.showIcon(IconNames.No)
         lastupdated = input.runningTime()
         serial.writeLine("AT")
         basic.pause(1000)
         serial.writeLine("AT+NRB")
-        basic.showIcon(IconNames.No)
     } else {
-        images.createImage(`
-            . . . . #
-            . . . . #
-            . . . # #
-            . . # # #
-            # # # # #
-            `).scrollImage(1, 100)
+        basic.showString("j")
     }
     basic.pause(200)
     basic.clearScreen()
-    basic.pause(2000)
+    basic.pause(5000)
 }
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    rx = serial.readLine()
-    rxPayload = cayenneLPP.extractPayloadStr(rx, "+NNMI:", ",")
-    if (rxPayload.length > 0) {
-        cayenneLPP.lpp_update(rxPayload[1])
-    } else {
-        rxPayload = cayenneLPP.extractPayloadStr(rx, "+NSMI:", ",")
-        if (rxPayload.length > 0) {
-            lastupdated = input.runningTime()
-            serial.writeLine("AT+SLEEP")
-        } else {
-            rxPayload = cayenneLPP.extractPayloadStr(rx, "+CGATT:", "")
-            if (rxPayload.length > 0) {
-                basic.showIcon(IconNames.Yes)
-                basic.pause(500)
-                basic.clearScreen()
-                connected = true
-            }
-        }
-    }
+    rx = serial.readUntil(serial.delimiters(Delimiters.NewLine))
+    extractPayload(rx.substr(0, rx.length - 1))
 })
 let txPayload = ""
 let vBat = 0
-let connected = false
-let rxPayload: string[] = []
 let rx = ""
+let rxPayload: string[] = []
 let lastupdated = 0
-led.setBrightness(20)
+let maxDelay = 0
+let connected = false
+led.setBrightness(10)
 pins.digitalWritePin(DigitalPin.P0, 0)
 cayenneLPP.add_digital(LPP_Direction.Output_Port, DigitalPin.P0)
 cayenneLPP.add_sensor(LPP_Bit_Sensor.Temperature)
@@ -56,8 +65,9 @@ BaudRate.BaudRate115200
 )
 serial.setWriteLinePadding(0)
 serial.setRxBufferSize(64)
-let dwellTime = 10000
-let maxDelay = 120000
+connected = true
+let dwellTime = 15000
+maxDelay = 120000
 let vRef = 1.236
 lastupdated = input.runningTime()
 basic.forever(function () {
